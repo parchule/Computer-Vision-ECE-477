acbba0e409a7ce07900a8eb1ff8beafe2af6d10f
parchule <parchule@purdue.edu>
2022-02-02 01:29:46 -0500

    Ubuntu 2

--- SyedAbdulGaffarShakhadri.py
+++ SyedAbdulGaffarShakhadri.py
@@ -40,33 +40,34 @@ class handDetector():
         self.trackCon = trackCon
         
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
+        if draw:
+            if self.results.multi_hand_landmarks:
+                for handLms in self.results.multi_hand_landmarks:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
     
     def findPosition(self, img, handNo=0, draw=True):
         lmList = []
-        if self.results.mult_hand_landmarks:
+        if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
+            for i in range(1, 6):
+                lm = myHand.landmark[i*4]
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append([id, cx, cy])
+                lmList.append([i*4, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
         return lmList 
 # Main function
 def main():
     cap = initCamera()
-    #print("USB Camera Check")
+    print("Camera Initialized")
     #cameraCheck(cap)
     pTime = 0
     cTime = 0
@@ -76,11 +77,11 @@ def main():
         success, img = cap.read()
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
-        #if len(lmList) != 0:
         
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
+        print(str(fps))
         
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         
